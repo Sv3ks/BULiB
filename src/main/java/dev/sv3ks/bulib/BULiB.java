d273aacebfaf4e13fca9c76663c244c7bd92455c
Sv3ks <89691080+Sv3ks@users.noreply.github.com>
2024-08-13 21:42:04 +0200

    Project Structure

---
+++ src/main/java/dev/sv3ks/bulib/BULiB.java
@@ -0,0 +1,17 @@
+package dev.sv3ks.bulib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BULiB extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
